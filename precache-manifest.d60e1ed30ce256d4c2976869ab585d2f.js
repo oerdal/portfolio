99f333a3ea77de53c5aace937c8a224f0fb32bda
Ozan Erdal <oerdal@D-10-19-187-54.dhcp4.washington.edu>
2018-11-14 15:37:59 -0800

    Updates

---
+++ precache-manifest.d60e1ed30ce256d4c2976869ab585d2f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b0288e3e75127baff8366fc117de6b1",
+    "url": "/portfolio/static/media/ozan.9b0288e3.jpeg"
+  },
+  {
+    "revision": "cd62aa8f468958a5803989e90975a66d",
+    "url": "/portfolio/static/media/iconmonstr-github.cd62aa8f.png"
+  },
+  {
+    "revision": "44bc8870ceecf21d8397323e79f54c60",
+    "url": "/portfolio/static/media/darkclouds.44bc8870.png"
+  },
+  {
+    "revision": "8317d6ddbcaca2e3e6d74fba13f2646d",
+    "url": "/portfolio/static/media/clouds.8317d6dd.png"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "b5e8fbc8bb96a4948042",
+    "url": "/portfolio/static/js/main.b5e8fbc8.chunk.js"
+  },
+  {
+    "revision": "38bd2b3b801e00187bf6",
+    "url": "/portfolio/static/js/1.38bd2b3b.chunk.js"
+  },
+  {
+    "revision": "b5e8fbc8bb96a4948042",
+    "url": "/portfolio/static/css/main.fbd1583b.chunk.css"
+  },
+  {
+    "revision": "4fadd1e6bed75fb61511fd1e8a731e46",
+    "url": "/portfolio/index.html"
+  }
+];
